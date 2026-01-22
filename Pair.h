b9d6deac52b35c5b68fb83554dcf92a456fe573f
ryanesp15 <0489698@MY.SCCCD.EDU>
2026-01-22 13:47:16 -0800

    Started implementation: added constructor.

--- Pair.h
+++ Pair.h
@@ -1,5 +1,5 @@
 // Pair.h
-// Your Names
+// Ryan Esparza
 #ifndef PAIR_H
 #define PAIR_H
 
@@ -9,11 +9,26 @@ private:
     T first;
     T second;
 public:
+
     Pair(T f, T s);
     T getFirst() const;
     T getSecond() const;
     void setFirst(T f);
     void setSecond(T s);
 };
+
+template <typename T>
+Pair<T>::Pair(T f, T s) {
+    first = f;
+    second = s;
+}
+
+
+
+
+main() {
+
+    Pair<int> pairOne(5, 6);
+}
 
 #endif
