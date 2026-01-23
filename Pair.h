a584537d98e68adc7cee2f6674e77b6b63c9e990
ryanesp15 <0489698@MY.SCCCD.EDU>
2026-01-22 20:32:17 -0800

    Final cleanup and comments.

--- Pair.h
+++ Pair.h
@@ -5,49 +5,52 @@
 
 template <typename T>
 class Pair {
+
 private:
-    T first;
-    T second;
+
+   T first;
+   T second;
+
 public:
 
-    Pair(T f, T s);
-    T getFirst() const;
-    T getSecond() const;
-    void setFirst(T f);
-    void setSecond(T s);
+   Pair(T f, T s);
+   T getFirst() const;
+   T getSecond() const;
+   void setFirst(T f);
+   void setSecond(T s);
 };
 
 template <typename T>
 Pair<T>::Pair(T f, T s) {
-    first = f;
-    second = s;
+   first = f;
+   second = s;
 }
 
 template <typename T>
 T Pair<T>::getFirst() const {
-    return f;
+   return f;
 }
 
 template <typename T>
 T Pair<T>::getSecond() const {
-    return S;
+   return S;
 }
 
 template <typename T>
 void Pair<T>::setFirst(T f) {
-    fisrt = f;
+   fisrt = f;
 }
 
 template <typename T>
 void Pair<T>::setSecond(T s) {
-    second = s;
+   second = s;
 }
 
 main() {
 
-    Pair<int> pairOne(1, 2);
+   Pair<int> pairOne(1, 2);
 
-    Pair<string> pairTwo("apple", "banana");
+   Pair<string> pairTwo("apple", "banana");
 }
 
 #endif
